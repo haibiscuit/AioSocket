a228f031a27eb652cf03037fccff13666777c1c2
haibiscuit <32199198+haibiscuit@users.noreply.github.com>
2019-05-10 18:05:19 +0800

    Update WriteCompletionHandler.java
    解决异步读bug

--- AioSocket/src/Client/WriteCompletionHandler.java
+++ AioSocket/src/Client/WriteCompletionHandler.java
@@ -14,21 +14,30 @@ import java.util.concurrent.CountDownLatch;
 public class WriteCompletionHandler implements CompletionHandler<Integer, ByteBuffer> {  
     private AsynchronousSocketChannel clientChannel;  
     private CountDownLatch latch;  
+    
+  
     public WriteCompletionHandler(AsynchronousSocketChannel clientChannel,CountDownLatch latch) {  
         this.clientChannel = clientChannel;  
         this.latch = latch;  
     }  
     @Override  
     public void completed(Integer result, ByteBuffer buffer) {  
+
+        AsyncClientHandler.semphore.release();        //异步写完释放锁
         //完成全部数据的写入  
-        if (buffer.hasRemaining()) {  
+        if (buffer.hasRemaining()) { 
+              try {
+                AsyncClientHandler.semphore.acquire();       //防止异步读
+            } catch (InterruptedException ex) {
+                Logger.getLogger(WriteCompletionHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
             clientChannel.write(buffer, buffer, this);  
         }  
-        else {  
+        //else {         //这里暂时删去，即客户端发送消息，但是接收不到消息
             //读取数据  
-            ByteBuffer readBuffer = ByteBuffer.allocate(1024);  
-            clientChannel.read(readBuffer,readBuffer,new ReadCompletionHandler(clientChannel, latch));  
-        }  
+          //  ByteBuffer readBuffer = ByteBuffer.allocate(1024);  
+           // clientChannel.read(readBuffer,readBuffer,new ReadCompletionHandler(clientChannel, latch));  
+        //}  
     }  
     @Override  
     public void failed(Throwable exc, ByteBuffer attachment) {  
